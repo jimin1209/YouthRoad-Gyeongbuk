b142974bd11162e36c56e6cb89995fd7f663e056
jimin1209 <adorable_min@naver.com>
2025-11-23 14:27:51 +0900

    Remove duplicate export directive from providers

--- lib/application/providers.dart
+++ lib/application/providers.dart
@@ -43,5 +43,3 @@ final memoProvider =
 
 final chatProvider =
     NotifierProvider.autoDispose<ChatNotifier, ChatState>(ChatNotifier.new);
-
-export 'notifiers/region_notifier.dart' show regionProvider, RegionNotifier;
