68de5ae9fbabfb303eb49587bff335481bed97e6
jimin1209 <adorable_min@naver.com>
2025-11-23 21:02:59 +0900

    Use injected Dio for institution and department repositories

--- lib/application/repository_providers.dart
+++ lib/application/repository_providers.dart
@@ -4,20 +4,16 @@ import '../data/models/dept_model.dart';
 import '../data/models/inst_model.dart';
 import '../data/repositories/dept_repository_impl.dart';
 import '../data/repositories/inst_repository_impl.dart';
-import '../data/sources/remote/dept_remote_source.dart';
-import '../data/sources/remote/inst_remote_source.dart';
+import '../data/sources/remote/department_remote_source.dart';
+import '../data/sources/remote/institution_remote_source.dart';
 import '../domain/repositories/dept_repository.dart';
 import '../domain/repositories/inst_repository.dart';
 import 'di.dart';
 
-final instRemoteSourceProvider = Provider<InstRemoteSource>((ref) {
-  final dio = ref.watch(dioProvider);
-  return InstRemoteSource(dio);
-});
-
 final instRepositoryProvider = Provider<InstRepository>((ref) {
-  final remote = ref.watch(instRemoteSourceProvider);
-  return InstRepositoryImpl(remote);
+  final dio = ref.watch(dioProvider);
+  final remoteSource = InstitutionRemoteSource(dio);
+  return InstRepositoryImpl(null, remoteSource);
 });
 
 final instListProvider = AutoDisposeFutureProvider<List<InstModel>>((ref) async {
@@ -25,14 +21,10 @@ final instListProvider = AutoDisposeFutureProvider<List<InstModel>>((ref) async 
   return repository.fetchInstList();
 });
 
-final deptRemoteSourceProvider = Provider<DeptRemoteSource>((ref) {
-  final dio = ref.watch(dioProvider);
-  return DeptRemoteSource(dio);
-});
-
 final deptRepositoryProvider = Provider<DeptRepository>((ref) {
-  final remote = ref.watch(deptRemoteSourceProvider);
-  return DeptRepositoryImpl(remote);
+  final dio = ref.watch(dioProvider);
+  final remoteSource = DepartmentRemoteSource(dio);
+  return DeptRepositoryImpl(null, remoteSource);
 });
 
 final deptListProvider = AutoDisposeFutureProvider.family<List<DeptModel>, String>(
