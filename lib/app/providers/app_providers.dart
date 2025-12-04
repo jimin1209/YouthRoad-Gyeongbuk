224a6a449b165588abcdff32e16816a78910ff8d
jimin1209 <adorable_min@naver.com>
2025-12-04 14:50:27 +0900

    Refactor router setup to use navigation version

--- lib/app/providers/app_providers.dart
+++ lib/app/providers/app_providers.dart
@@ -3,7 +3,7 @@ import 'package:go_router/go_router.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import '../../application/di.dart';
-import '../router/app_router.dart';
+import '../../navigation/app_router.dart';
 
 final appRouterProvider = Provider<GoRouter>((ref) {
   return const AppRouter().router();
